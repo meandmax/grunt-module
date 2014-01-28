b1bb35a88e238fb4784081f33e70e6be51d7be79
Clemens Akens <clebert@me.com>
2014-01-28 13:12:02 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function (grunt) {
             file: 'package.json'
         },
         jshint: {
-            js: {
+            'lint-js': {
                 options: {
                     jshintrc: '.jshintrc'
                 },
@@ -15,7 +15,7 @@ module.exports = function (grunt) {
                     '!node_modules/**/*.js'
                 ]
             },
-            json: {
+            'lint-json': {
                 src: [
                     '**/*.json',
                     '!node_modules/**/*.json'
@@ -23,12 +23,12 @@ module.exports = function (grunt) {
             }
         },
         module: {
-            check: {
+            'check-repository': {
                 options: {
                     check: true
                 }
             },
-            license: {
+            'license-copyright': {
                 options: {
                     replace: true,
                     line: 3
@@ -52,15 +52,15 @@ module.exports = function (grunt) {
     grunt.registerTask('default', 'lint');
 
     grunt.registerTask('lint', [
-        'jshint:js',
-        'jshint:json'
+        'jshint:lint-js',
+        'jshint:lint-json'
     ]);
 
     grunt.registerTask('publish', function (type) {
         grunt.task.run('lint');
-        grunt.task.run('module:check');
+        grunt.task.run('module:check-repository');
         grunt.task.run('bumpup:' + (type || 'patch'));
-        grunt.task.run('module:license');
+        grunt.task.run('module:license-copyright');
         grunt.task.run('module:release-publish');
     });
 
