681a481d3c92b1795bed55766a873526222ebcd8
Clemens Akens <clebert@me.com>
2014-01-28 01:36:07 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,31 @@
 
 module.exports = function (grunt) {
     grunt.initConfig({
+        bumpup: {
+            options: {
+                updateProps: {
+                    pkg: 'package.json'
+                }
+            },
+            file: 'package.json'
+        },
+        jshint: {
+            js: {
+                options: {
+                    jshintrc: '.jshintrc'
+                },
+                src: [
+                    '**/*.js',
+                    '!node_modules/**/*.js'
+                ]
+            },
+            json: {
+                src: [
+                    '**/*.json',
+                    '!node_modules/**/*.json'
+                ]
+            }
+        },
         module: {
             check: {
                 options: {
@@ -24,10 +49,22 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-bumpup');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
     grunt.loadTasks('tasks');
 
-    grunt.registerTask('default', [
-        'module:license',
-        'module:check'
+    grunt.registerTask('default', 'lint');
+
+    grunt.registerTask('lint', [
+        'jshint:js',
+        'jshint:json'
     ]);
+
+    grunt.registerTask('release', function (type) {
+        grunt.task.run('lint');
+        grunt.task.run('bumpup:' + (type || 'patch'));
+    });
+
+    grunt.registerTask('travis', 'lint');
 };
